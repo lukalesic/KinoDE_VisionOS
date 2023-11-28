8b990164588d3e99e84599ae3b03daab8f2394e1
lukalesic <lukalesic20@gmail.com>
2023-11-28 10:02:17 +0100

    Initial Commit

---
+++ KinoVisionTest/KinoVisionTestApp.swift
@@ -0,0 +1,21 @@
+//
+//  KinoVisionTestApp.swift
+//  KinoVisionTest
+//
+//  Created by Luka Lešić on 28.11.2023..
+//
+
+import SwiftUI
+
+@main
+struct KinoVisionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
