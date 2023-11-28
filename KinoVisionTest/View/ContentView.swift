8270f7da5c82efa658ef4e288cbbe26651489a81
lukalesic <lukalesic20@gmail.com>
2023-11-28 11:02:17 +0100

    setup views

--- KinoVisionTest/View/ContentView.swift
+++ KinoVisionTest/View/ContentView.swift
@@ -1,55 +1,26 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  KinoVisionTest
 //
 //  Created by Luka Lešić on 28.11.2023..
 //
 
 import SwiftUI
-import RealityKit
-import RealityKitContent
 
 struct ContentView: View {
-
-    @State private var showImmersiveSpace = false
-    @State private var immersiveSpaceIsShown = false
-
-    @Environment(\.openImmersiveSpace) var openImmersiveSpace
-    @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
-
     var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
-
-            Toggle("Show Immersive Space", isOn: $showImmersiveSpace)
-                .toggleStyle(.button)
-                .padding(.top, 50)
-        }
-        .padding()
-        .onChange(of: showImmersiveSpace) { _, newValue in
-            Task {
-                if newValue {
-                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
-                    case .opened:
-                        immersiveSpaceIsShown = true
-                    case .error, .userCancelled:
-                        fallthrough
-                    @unknown default:
-                        immersiveSpaceIsShown = false
-                        showImmersiveSpace = false
-                    }
-                } else if immersiveSpaceIsShown {
-                    await dismissImmersiveSpace()
-                    immersiveSpaceIsShown = false
+        TabView {
+            ForYouView()
+                .tabItem {
+                    Label("For you", systemImage: "star")
                 }
-            }
-        }
-    }
+            WhatToWatchView()
+                .tabItem {
+                    Label("What to watch", systemImage: "play.circle")
+                }
+        }    }
 }
 
-#Preview(windowStyle: .automatic) {
+#Preview {
     ContentView()
 }
---
+++ KinoVisionTest/View/WhatToWatchView.swift
@@ -0,0 +1,18 @@
+//
+//  WhatToWatchView.swift
+//  KinoVisionTest
+//
+//  Created by Luka Lešić on 28.11.2023..
+//
+
+import SwiftUI
+
+struct WhatToWatchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WhatToWatchView()
+}
